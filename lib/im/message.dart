2932968c475100954278a55cb0c8b1578092b259
dengzii <dengzii@foxmail.com>
2022-07-18 16:07:07 +0800

    update im model

--- lib/im/message.dart
+++ lib/im/message.dart
@@ -1,3 +1,64 @@
-abstract class Message {
+import 'package:json_annotation/json_annotation.dart';
 
+part 'message.g.dart';
+
+enum Action {
+  none(action: ""),
+
+  // chat
+  messageChat(action: "message.chat"),
+  messageChatResend(action: "message.chat.resend"),
+  messageGroupChat(action: "message.group"),
+  messageFailed(action: "message.failed.send"),
+
+  // ack
+  ack(action: "ack.message"),
+  ackNotify(action: "ack.notify"),
+  ackRequest(action: "ack.request"),
+
+  // control
+  apiAuth(action: "api.auth"),
+  heartbeat(action: "heartbeat"),
+
+  // notify
+  notifyUnknownAction(action: "notify.unknown.action"),
+  notifyKickout(action: "notify.kickout"),
+  notifyNewContact(action: "notify.contact"),
+  notifyError(action: "notify.error"),
+  hello(action: "hello"),
+  notify(action: "notify");
+
+  final String action;
+
+  const Action({required this.action});
+}
+
+@JsonSerializable()
+class Message {
+  String id = "";
+  String from = "";
+  String to = "";
+  String content = "";
+  int type = 0;
+  int sendAt = 0;
+
+  Message({
+    required this.id,
+    required this.from,
+    required this.to,
+    required this.content,
+    required this.type,
+    required this.sendAt,
+  });
+}
+
+@JsonSerializable()
+class CommonMessage {
+  String action = "";
+  String from = "";
+  String to = "";
+  int seq = 0;
+
+  dynamic data;
+  Map<String, dynamic>? extra;
 }
--- lib/im/sessions.dart
+++ lib/im/sessions.dart
@@ -1,7 +1,17 @@
 import 'package:dart_sdk/im/session.dart';
 
+enum UpdateType {
+  add,
+  remove,
+  update,
+}
+
+typedef UpdateListener = void Function(UpdateType type, Session session);
+
 abstract class SessionList {
-  Future<Session> getSession(String id);
+  bool isExist(String id);
+
+  Future<Session?> getSession(String id);
 
   Future<Session> createSession(String id);
 
@@ -9,5 +19,72 @@ abstract class SessionList {
 
   Future<List<Session>> getSessions();
 
-  void subscribeSessionUpdate(void Function(Session session) callback);
+  void onUpdate(UpdateListener listener);
+
+  void removeUpdate(UpdateListener listener);
+}
+
+class SessionListImpl implements SessionList {
+
+  final Map<String, Session> _sessions = {};
+  final List<UpdateListener> _listeners = [];
+  final List<Session> _sessionsList = [];
+
+  @override
+  bool isExist(String id) {
+    return _sessions.containsKey(id);
+  }
+
+  @override
+  Future<Session?> getSession(String id) async {
+    if (_sessions.containsKey(id)) {
+      return _sessions[id];
+    }
+    return null;
+  }
+
+  @override
+  Future<Session> createSession(String id) async {
+    if (_sessions.containsKey(id)) {
+      return _sessions[id]!;
+    }
+    final session = SessionImpl(this, id);
+    _sessions[id] = session;
+    _sessionsList.add(session);
+    _notifyUpdate(UpdateType.add, session);
+    return session;
+  }
+
+  @override
+  Future<void> deleteSession(String id, {bool reserveHistory = false}) async {
+    if (!_sessions.containsKey(id)) {
+      return;
+    }
+    final session = _sessions[id]!;
+    _sessions.remove(id);
+    _sessionsList.remove(session);
+    _notifyUpdate(UpdateType.remove, session);
+    await session.delete(reserveHistory: reserveHistory);
+  }
+
+  @override
+  Future<List<Session>> getSessions() async {
+    return _sessionsList;
+  }
+
+  @override
+  void onUpdate(UpdateListener listener) {
+    _listeners.add(listener);
+  }
+
+  @override
+  void removeUpdate(UpdateListener listener) {
+    _listeners.remove(listener);
+  }
+
+  void _notifyUpdate(UpdateType type, Session session) {
+    for (final listener in _listeners) {
+      listener(type, session);
+    }
+  }
 }
